0b2471eca6cd981d166b07dbb6ccd34a0ec082ee
itcentersk <33276114+itcentersk@users.noreply.github.com>
2018-03-22 21:29:08 +0700

    Initial commit

---
+++ app/src/main/java/com/example/sunun/sunun2app/Sunun2MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.sunun.sunun2app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Sunun2MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sunun2_main);
+    }
+}
